67eefda8eba8b14c28aae50f17148b131df3f0e8
shivam-1205 <148266278+shivam-1205@users.noreply.github.com>
2025-02-27 23:48:31 +0530

    new portfolio

---
+++ src/content_option.js
@@ -0,0 +1,176 @@
+
+const logotext = "Shivam Singh";
+const meta = {
+    title: "Shivam singh",
+    description: "I’m Shivam Singh, a Full Stack Developer currently working at Nerdtech.",
+};
+
+const introdata = {
+    title: "I’m Shivam Singh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop api",
+    },
+    description: "Hello! I’m Shivam Singh, a passionate and dedicated Web Developer. I specialize in building dynamic and responsive websites using technologies like Javascript, React,Node.js, and MySQL. With a strong foundation in web development, I am skilled at crafting user-friendly and efficient solutions that bring ideas to life on the web.",
+    
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hi, I’m Shivam Singh, a passionate and driven Web Developer with expertise in HTML, CSS, Node.js, and MySQL. I enjoy designing and building user-friendly websites and applications that are both functional and visually appealing. With a strong foundation in web development, I focus on creating efficient solutions and writing clean, maintainable code. I’m constantly learning and staying updated with the latest technologies to improve my skills and deliver the best results.I’m particularly good at front-end development, back-end integrations, and database management, making me well-equipped to handle the complete web development process.In my free time, I love exploring new frameworks, solving coding challenges, and contributing to interesting projects. Let’s create something amazing together!.",
+};
+const 
+
+Education = [{
+        jobtitle: "11 & 12",
+        where: "Maharashtra Board HSC",
+        date: "2017 To 2019",
+    },
+    {
+        jobtitle: "Bachelor in BA",
+        where: "Pune university",
+        date: "2019 To 2022",
+    },
+    {
+        jobtitle: "Master in Computer Applications (MCA)",
+        where: "Pune university",
+        date: "2022 To 2024",
+    },
+];
+
+const skills = [{
+        name: "React js",
+        value: 90,
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "Node js",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "Tailwind",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Website Development",
+        description: <> Custom website design and development tailored to your business needs.<br/> Responsive and mobile-friendly designs for a seamless user experience on any device. </>,
+    },
+    {
+        title: "Front-End Development",
+        description: <>Creating interactive and visually appealing user interfaces using HTML, CSS, and JavaScript React ,redux.<br/>Optimizing websites for performance and accessibility.</>,
+    },
+    {
+        title: "Back-End Development",
+        description: <>Building secure and scalable server-side applications with Node.js, express.js.<br/>API development and integration for smooth functionality.</>,
+    },
+    {
+        title: "Database Management",
+        description: <>Designing, implementing, and optimizing databases with MySQL.<br/>Ensuring data integrity and security for your applications.</>,
+    },
+    {
+        title: "Full-Stack Development",
+        description: <>End-to-end development solutions, from front-end to back-end and database management.<br/>Complete project handling and deployment.</>,
+    },
+    {
+        title: "Maintenance and Support",
+        description: <>Regular updates and maintenance for your website or application.<br/>Technical support to ensure everything runs smoothly.</>,
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shivamjsingh1205@gmail.com",
+    YOUR_FONE: "+91 8208746919",
+    description: "I’d love to hear from you! Whether you have a project idea, need help with web development, or just want to say hello, feel free to reach out.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_1t3rj3f",
+    YOUR_TEMPLATE_ID: "template_zbklt4s",
+    YOUR_USER_ID: "hggkGl2NFJCZLaPko",
+};
+
+const socialprofils = {
+    github: "https://github.com/shivam-1205",
+    facebook: "https://www.instagram.com/shivamsingh_.rajput?igsh=MWRjN2IyODdja3FpaQ==",
+    linkedin: "https://www.linkedin.com/in/shivam-singh-1a980428b",
+    twitter: "https://x.com/ShivamS1205?t=YwblPgWSqedUEk9YHabGLQ&s=08",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio,
+
+    Education, introdata, logotext, meta, services, skills, socialprofils
+};
+
